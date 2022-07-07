4d69db0bf7145354ff3408e58d2952d02a65beba
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-07-07 12:13:46 -0300

    segundo commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 class Categoria(models.Model):
@@ -17,9 +18,18 @@ class Autor(models.Model):
     def __str__(self):
         return self.name.capitalize()
 
+    class Meta:
+        verbose_name_plural = "Autores"
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)   
     ISBN = models.CharField(max_length=32)
     quantidade = models.IntegerField()
-    preco = models.DecimalField(max_length=7, decimal_places=2)
-    categoria = models.ForeignKey()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+
+    def __str__(self):
+        return f'{self.titulo} ({self.quantidade})'
+
+
