9018c2323a485e283146bcf2aa0388cb38884606
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-07-08 09:15:10 -0300

    aplicação do rest framework

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from core.models import Categoria
+from core.serializers import CategoriaSerializer
 
-# Create your views here.
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
